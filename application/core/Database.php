33c0cc18cfc8a730a153df1872622b7909e2c893
tymur97@gmail.com <t.aysin@nextcontact.ru>
2021-07-26 17:08:42 +0300

    class Database(singleton) for db connection instead creation connection in Model class

---
+++ application/core/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Core;
+use PDO;
+class Database {
+
+    private static $connection = null;
+
+    const USER = 'root';
+    const PASS = '';
+    const HOST = 'localhost';
+    const DB = 'Books';
+
+    public static function getConnect()
+    {
+        $user = self::USER;
+        $pass = self::PASS;
+        $host = self::HOST;
+        $db = self::DB;
+
+        if (null === self::$connection) {
+            self::$connection = new PDO("mysql:dbname=$db;host=$host",$user,$pass);
+        }
+        return self::$connection;
+    }
+
+    private function __clone() {}
+    private function __construct() {}
+
+
+}
--- application/core/Model.php
+++ application/core/Model.php
@@ -1,22 +1,11 @@
 <?php
 namespace Core;
-use PDO;
+use Core\Database;
     class Model
     {
 
-            const USER = 'root';
-            const PASS = '';
-            const HOST = 'localhost';
-            const DB = 'Books';
-
         public function connect_to_db()
         {
-            $user = self::USER;
-            $pass = self::PASS;
-            $host = self::HOST;
-            $db = self::DB;
-
-            return $connection = new PDO("mysql:dbname=$db;host=$host",$user,$pass);
-
+            return Database::getConnect();
         }
     }
